d1c4fd1d01eefed0cf74658da6af13dac487f08a
adrianplusplus <agalicia@rohan.sdsu.edu>
2016-05-26 11:22:30 -0500

    refactor view content and reboostrap angularjs
    - removed the content of index
    - boostrap the app with index.htm
    - created 2 routes(ui.router) and 2 controllers

--- app.js
+++ app.js
@@ -3,12 +3,38 @@
 (function () {
     'use strict';
 
-    var app = angular.module('practiceApp', ['ui.bootstrap']);
+    var app = angular.module('practiceApp', ['ui.router', 'ui.bootstrap']);
+
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'partials/tables.html',
+                controller:'HomeCtrl'
+            })
+            .state('secondary', {
+                url: '/secondary',
+                templateUrl: 'partials/secondary.html',
+                controller:'SecondaryCtrl'
+            })
+
+    })
 
     app.controller('HomeCtrl', ['$scope', function ($scope) {
-        console.log('loaded Ctrl');
+        console.log('loaded home Ctrl');
         $scope.greet = "Hello World";
-        $scope.students  = [{fn:'adrian', ln:'galicia'}, {fn:'rosario',ln:'marcial'}, {fn:'jim', ln:'carrey'}]
+        $scope.students = [
+            { fn: 'adrian', ln: 'galicia'}, 
+            { fn: 'rosario', ln: 'marcial' },
+            { fn: 'jim', ln: 'carrey'}
+        ];
+        
+    }]);
+    app.controller('SecondaryCtrl',['$scope',function($scope){
+        console.log('loaded secondaryCtrl');
+        
     }]);
 
 }());
